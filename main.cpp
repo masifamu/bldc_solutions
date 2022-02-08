054c6cfe18062191e561d9ccf1f6223542eb8a25
masifsr <asif@sevenrobotics.in>
2022-02-09 00:49:57 +0530

    adding a greeting.txt in resources dir

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include<cstdio>
+#include<iostream>
+#include<fstream>
+#include<string>
 
 int main(void){
-	printf("hello world\n");
+	std::ifstream greetingFile("/resources/greeting.txt");
+	if(greetingFile.is_open()){
+		std::string greetingText;
+		getline(greetingFile, greetingText);
+		std::cout<<greetingText;
+	}else{
+		std::cout<<"Error: "<< greetingFile.rdstate();
+	}
 	return 0;
 }
